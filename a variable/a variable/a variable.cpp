214b119388a14972759e89113d56b79a9325ffb2
HanLu <146564137+HanLu-ai@users.noreply.github.com>
2025-06-25 17:11:25 +0800

    Add files via upload

---
+++ a variable/a variable/a variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "欢迎使用御风工作室教程" << endl;
+	//变量的定义
+	//语法：数据类型 变量名 = 初始值
+	int a = 10;
+	cout << "a=" << a << endl;
+	system("pause");
+	return 0;
+}
+//注意：C++在创建变量时，必须给变量一个初始值，否则会报错
